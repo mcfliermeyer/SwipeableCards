5af5057fcdc81de54eeb6e579bcb51b149fa75c5
Mark Meyer <mcfliermeyer@gmail.com>
2022-12-30 06:45:09 -0500

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Animated,
+} from "react-native";
+
+const SCREEN_HEIGHT = Dimensions.get("window").height;
+const SCREEN_WIDTH = Dimensions.get("window").width;
 
 export default function App() {
+  const Foods = [
+    { id: "1", uri: require("./assets/1.jpg") },
+    { id: "2", uri: require("./assets/2.jpg") },
+    { id: "3", uri: require("./assets/3.jpg") },
+    { id: "4", uri: require("./assets/4.jpg") },
+    { id: "5", uri: require("./assets/5.jpg") },
+  ];
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <View style={{ height: 60 }}></View>
+      <View style={{ flex: 1 }}>
+        <Animated.View
+          style={[
+            {
+              height: SCREEN_HEIGHT - 120,
+              width: SCREEN_WIDTH,
+              padding: 10,
+            },
+          ]}
+        >
+          <Image
+            style={{
+              flex: 1,
+              width: "100%",
+              height: "100%",
+              resizeMode: "cover",
+              borderRadius: 20,
+            }}
+            source={Foods[0].uri}
+          />
+        </Animated.View>
+      </View>
+      <View style={{ height: 60 }}></View>
     </View>
   );
 }
@@ -13,8 +52,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
