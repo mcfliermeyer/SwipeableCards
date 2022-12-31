55377a916410950896f02137110a83b2d80cad8e
Mark Meyer <mcfliermeyer@gmail.com>
2022-12-31 13:34:17 -0500

    Top card animation for movement working cleanly.

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { StatusBar } from "expo-status-bar";
+import { useRef, useState } from "react";
 import {
   Dimensions,
   StyleSheet,
@@ -13,40 +14,86 @@ const SCREEN_HEIGHT = Dimensions.get("window").height;
 const SCREEN_WIDTH = Dimensions.get("window").width;
 
 export default function App() {
+  const [cardIndex, setCardIndex] = useState(2);
   const Foods = [
     { id: "1", uri: require("./assets/1.jpg") },
     { id: "2", uri: require("./assets/2.jpg") },
     { id: "3", uri: require("./assets/3.jpg") },
-    { id: "4", uri: require("./assets/4.jpg") },
-    { id: "5", uri: require("./assets/5.jpg") },
+    // { id: "4", uri: require("./assets/4.jpg") },
+    // { id: "5", uri: require("./assets/5.jpg") },
   ];
 
   const renderFoods = () => {
     return Foods.map((item, i) => {
-      return (
-        <Animated.View
-          key={i}
-          style={{
-            height: SCREEN_HEIGHT - 120,
-            width: SCREEN_WIDTH,
-            padding: 10,
-            position: "absolute",
-          }}
-        >
-          <Image
+      if (i === cardIndex) {
+        return (
+          <Animated.View
+            key={i}
             style={{
-              flex: 1,
-              width: "100%",
-              height: "100%",
-              resizeMode: "cover",
-              borderRadius: 20,
+              height: SCREEN_HEIGHT - 120,
+              width: SCREEN_WIDTH,
+              padding: 10,
+              position: "absolute",
+              transform: [{ translateX: pan.x }, { translateY: pan.y }],
             }}
-            source={item.uri}
-          />
-        </Animated.View>
-      );
+            {...panResponder.panHandlers}
+          >
+            <Image
+              style={{
+                flex: 1,
+                width: "100%",
+                height: "100%",
+                resizeMode: "cover",
+                borderRadius: 20,
+              }}
+              source={item.uri}
+            />
+          </Animated.View>
+        );
+      } else {
+        return (
+          <Animated.View
+            key={i}
+            style={{
+              height: SCREEN_HEIGHT - 120,
+              width: SCREEN_WIDTH,
+              padding: 10,
+              position: "absolute",
+            }}
+          >
+            <Image
+              style={{
+                flex: 1,
+                width: "100%",
+                height: "100%",
+                resizeMode: "cover",
+                borderRadius: 20,
+              }}
+              source={item.uri}
+            />
+          </Animated.View>
+        );
+      }
     });
   };
+  const pan = useRef<any>(new Animated.ValueXY()).current;
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderGrant: () => {
+        pan.setOffset({
+          x: pan.x._value,
+          y: pan.y._value,
+        });
+      },
+      onPanResponderMove: Animated.event([null, { dx: pan.x, dy: pan.y }], {
+        useNativeDriver: false,
+      }),
+      onPanResponderRelease: () => {
+        pan.flattenOffset();
+      },
+    })
+  ).current;
   return (
     <View style={{ flex: 1 }}>
       <View style={{ height: 60 }}></View>
