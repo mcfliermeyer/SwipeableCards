48bcc80d7bd5a1190744914be5adc52d3746eec8
Mark Meyer <mcfliermeyer@gmail.com>
2023-01-04 14:55:46 -0500

    Change location of bottom 2 cards to be offset and ease-in-out animation.

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Dimensions, View, Image, Animated, PanResponder } from "react-native";
 
 const SCREEN_HEIGHT = Dimensions.get("window").height;
@@ -12,16 +12,29 @@ let data = [
 type CardProps = {
   item: { id: string; uri: any };
   moveAnyCardToBottom: (id: string) => void;
+  index: number;
 };
 
-const SwipeableCard = ({ item, moveAnyCardToBottom }: CardProps) => {
-  const [yPosition, _] = useState(new Animated.Value(0));
+const SwipeableCard = ({ item, moveAnyCardToBottom, index }: CardProps) => {
+  const yPosition = useRef(new Animated.Value(0)).current;
+
   const rotateCard = yPosition.interpolate({
     //gives card slight rotation when swiping
     inputRange: [-SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2],
     outputRange: ["-10deg", "0deg", "10deg"],
     extrapolate: "clamp",
   });
+  let offset = 0;
+  if (index == 0) offset -= 35;
+  else if (index == 1) offset -= 18;
+  const offsetAnimation = () => {
+    Animated.spring(yPosition, {
+      toValue: offset,
+      speed: 5,
+      useNativeDriver: false,
+    }).start();
+  };
+  offsetAnimation()
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => false,
     onMoveShouldSetPanResponder: () => true,
@@ -53,6 +66,7 @@ const SwipeableCard = ({ item, moveAnyCardToBottom }: CardProps) => {
             useNativeDriver: false,
           }).start();
         } else {
+          //swiped down
           Animated.timing(yPosition, {
             toValue: SCREEN_HEIGHT,
             duration: 200,
@@ -60,17 +74,10 @@ const SwipeableCard = ({ item, moveAnyCardToBottom }: CardProps) => {
           }).start();
         }
         moveAnyCardToBottom(item.id); //moves card to front of array and bottom of deck
-
-        Animated.spring(yPosition, {
-          //animate the card that was just moved to bottom center of deck
-          toValue: 0,
-          speed: 3,
-          bounciness: 12,
-          useNativeDriver: false,
-        }).start();
       }
     },
   });
+
   return (
     <Animated.View
       {...panResponder.panHandlers}
@@ -79,7 +86,10 @@ const SwipeableCard = ({ item, moveAnyCardToBottom }: CardProps) => {
         width: SCREEN_WIDTH,
         padding: 10,
         position: "absolute",
-        transform: [{ translateY: yPosition }, { rotate: rotateCard }],
+        transform: [
+          { translateY: yPosition },
+          { rotate: rotateCard },
+        ],
       }}
     >
       <Image
@@ -115,8 +125,8 @@ export default function App() {
         <SwipeableCard
           key={item.id}
           item={item}
+          index={i}
           moveAnyCardToBottom={() => moveAnyCardToBottom(item.id)}
-          // swipedDirection={() => swipeDirection}
         />
       );
     });
