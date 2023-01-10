992e62501f680de182dde153ecbf6ac2df23f0f8
Mark Meyer <mcfliermeyer@gmail.com>
2023-01-10 06:59:29 -0500

    Abstracted and optimized card swiping for navigation.

--- App.tsx
+++ App.tsx
@@ -1,49 +1,39 @@
-import { ReactNode, useEffect, useRef, useState } from "react";
-import { Dimensions, View, Image, Animated, PanResponder } from "react-native";
-import SwipeableCard from "./components/SwipeableCard";
-
-
-let data = [
-  { id: "1", uri: require("./assets/1.jpg") },
-  { id: "2", uri: require("./assets/2.jpg") },
-  { id: "3", uri: require("./assets/3.jpg") },
-];
-
-
-
-
-
+import { View} from "react-native";
+import SwipeableCardDeck from "./components/SwipeableCardDeck";
 export default function App() {
-  const [cardArray, setCardArray] = useState(data);
-
-  const moveTopCardToBottom = () => {
-    //move card from anywhere in deck to front of array using unshift
-    //first card to display is last in array
-    let cardsCopy = [...cardArray]; //shallow copy of array to mutate
-    const cardToMove = cardsCopy.pop() //locate card with id
-    cardsCopy.unshift(cardToMove!); //put card in array front which is bottom of the deck
-    setCardArray(() => cardsCopy); //change state for re-render
-  };
-  //create stacked cards that will be able to drag around top card
-  const renderCardsFromData = () => {
-    return cardArray.map((item, i) => {
-      return (
-        <SwipeableCard
-          key={item.id}
-          item={item}
-          index={i}
-          moveTopCardToBottom={() => moveTopCardToBottom()}
-        ></SwipeableCard>
-      );
-    });
-  };
-
-  //App Views
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ height: 60 }}></View>
-      <View style={{ flex: 1 }}>{renderCardsFromData()}</View>
-      <View style={{ height: 60 }}></View>
+    <View style={{ flex: 1, padding: 10 }}>
+      <View style={{ height: 90 }}></View>
+      <SwipeableCardDeck>
+        <View
+          style={{
+            flex: 1,
+            width: 200,
+            height: 200,
+            borderRadius: 20,
+            backgroundColor: "red",
+          }}
+        />
+        <View
+          style={{
+            flex: 1,
+            width: "100%",
+            height: "100%",
+            borderRadius: 20,
+            backgroundColor: "blue",
+          }}
+        />
+        <View
+          style={{
+            flex: 1,
+            width: 200,
+            height: 200,
+            borderRadius: 20,
+            backgroundColor: "orange",
+          }}
+        />
+      </SwipeableCardDeck>
+      <View style={{ height: 20 }}></View>
     </View>
   );
 }
--- components/SwipeableCardDeck.tsx
+++ components/SwipeableCardDeck.tsx
@@ -1,20 +1,130 @@
-import { ReactNode } from 'react'
-import { View, Text } from 'react-native'
-import SwipeableCard from './SwipeableCard';
+import { Children, ReactNode, useEffect, useRef, useState } from "react";
+import {
+  View,
+  Animated,
+  Dimensions,
+  PanResponder,
+} from "react-native";
+const SCREEN_HEIGHT = Dimensions.get("window").height;
+const SCREEN_WIDTH = Dimensions.get("window").width;
 type Props = {
   children: ReactNode[];
-}
-const SwipeableCardDeck = ({children, ...props}: Props) => {
-  // moveTopCardToBottom={() => moveTopCardToBottom()}
+};
+const SwipeableCardDeck = ({ children }: Props) => {
+  const childArray = Children.toArray(children); //ensures children will be array
+
+  //then move guaranteed children array into state
+  const [cardArray, setCardArray] = useState(
+    childArray.map((component) => cardBuilder(component))
+  );
+  const BASE = -25;
+
+  //whenever state changes, bring all cards back to correct position in an offset deck
+  useEffect(() => {
+    cardArray.map((card, index) => {
+      const offset = BASE + 9 * index + index * 30;
+      Animated.spring(card.yPosition, {
+        toValue: offset - 32,
+        speed: 5,
+        useNativeDriver: false,
+      }).start();
+    });
+  }, [cardArray]);
+
+  //gives card slight rotation when swiping vertically
+  const rotateCard = (yPosition: Animated.Value) => {
+    return yPosition.interpolate({
+      inputRange: [-SCREEN_WIDTH / 2, 10, SCREEN_WIDTH / 2],
+      outputRange: ["-15deg", "0deg", "10deg"],
+      extrapolate: "clamp",
+    });
+  };
+
+  //gives each card a seperate gesture reader and seperate y coordinates to keep track of itself
+  //when card is swiped far enough, triggers state change which moves card to back of deck
+  function cardBuilder(component: ReactNode) {
+    const yPosition = useRef(new Animated.Value(0)).current;
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => false,
+      onMoveShouldSetPanResponder: () => true,
+      onStartShouldSetPanResponderCapture: () => false,
+      onMoveShouldSetPanResponderCapture: () => true,
+      onPanResponderMove: (_, gestureState) => {
+        yPosition.setValue(gestureState.dy);
+      },
+      onPanResponderRelease: (_, gestureState) => {
+        if (
+          // if swipe up or down isnt far enough, bring back to center
+          gestureState.dy > -SCREEN_HEIGHT / 2 + 200 &&
+          gestureState.dy < SCREEN_HEIGHT / 2 - 200
+        ) {
+          //animates card back to center
+          Animated.spring(yPosition, {
+            toValue: 0,
+            speed: 5,
+            bounciness: 5,
+            useNativeDriver: false,
+          }).start();
+        } else {
+          //swipe was far enough up or down, move card out
+          if (gestureState.dy < -SCREEN_HEIGHT / 2 + 200) {
+            //swiped up
+            Animated.timing(yPosition, {
+              toValue: -SCREEN_HEIGHT,
+              duration: 200,
+              useNativeDriver: false,
+            }).start();
+          } else {
+            //swiped down
+            Animated.timing(yPosition, {
+              toValue: SCREEN_HEIGHT,
+              duration: 200,
+              useNativeDriver: false,
+            }).start();
+          }
+          // moveTopCardToBottom(cardArray);
+          setCardArray((prevDeck) => {
+            let copyDeck = [...prevDeck];
+            const cardToMove = copyDeck.pop()!;
+            copyDeck.unshift(cardToMove);
+            return copyDeck;
+          });
+        }
+      },
+    });
+    return {
+      component: component,
+      panResponder: panResponder,
+      yPosition: yPosition,
+    };
+  }
 
   return (
-    <View style={{ flex: 1 }}>
-      {children.map((component, index) => (
-        <SwipeableCard key={index} index={index}>
-          {component}
-        </SwipeableCard>
+    <View style={{ flex: 1, marginHorizontal: 10 }}>
+      {cardArray.map((component, index) => (
+        <Animated.View
+          {...cardArray[index].panResponder.panHandlers}
+          key={index}
+          style={{
+            height: SCREEN_HEIGHT - 140,
+            width: SCREEN_WIDTH - 40,
+            padding: 10,
+            position: "absolute",
+            backgroundColor: "gray",
+            borderRadius: 20,
+            borderColor: "black",
+            borderStyle: "solid",
+            borderWidth: 2,
+            transform: [
+              { translateY: cardArray[index].yPosition },
+              { rotate: rotateCard(cardArray[index].yPosition) },
+            ],
+          }}
+        >
+          {component.component}
+        </Animated.View>
       ))}
     </View>
   );
-}
-export default SwipeableCardDeck
+};
+export default SwipeableCardDeck;
